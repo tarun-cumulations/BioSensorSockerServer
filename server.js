f505ab6c534046d52fc1215740a6ee238a37d89c
tarun-cumulations <tarun.t@cumulations.com>
2023-05-02 18:46:26 +0530

    adding server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const PORT = 5772;
+
+const server = net.createServer((socket) => {
+  console.log('Client connected');
+
+  // handle incoming data from the biosensor
+  socket.on('data', (data) => {
+    console.log('Received data:', data.toString());
+    socket.write('Received data successfully\n');
+  });
+
+  // handle client disconnection
+  socket.on('end', () => {
+    console.log('Client disconnected');
+  });
+});
+
+// start the server and listen on the specified port
+server.listen(PORT ,() => {
+  console.log("Server up and runninga at 5772");
+});
